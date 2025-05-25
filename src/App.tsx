ce63bbea2ac61f72963338e55a385491889171cf
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-05-25 09:22:30 +0800

    Default URL to deployed server

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [input, setInput] = useState('')
 
   const sendMessage = async (message: string) => {
-    const apiUrl: string = import.meta.env.VITE_API_URL || '';
+    const apiUrl: string = import.meta.env.VITE_API_URL || 'https://tran-llm-daatfkc6hhf0a8hf.southeastasia-01.azurewebsites.net/openai/question';
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
