9838f9a9a02f289b7877937c302ec594510e6c24
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-08 22:45:08 +0800

    Extract content box CSS

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,7 +7,7 @@ const Login: React.FC = () => {
   const { instance } = useMsal();
   return (
     <div className="login-center-container h-full flex flex-col justify-center items-center">
-      <div className="bg-gray-800 border border-gray-700 rounded-lg p-8 flex flex-col justify-center items-center text-center shadow-[0_0_8px_rgba(255,255,255,0.7)]">
+      <div className="content-box">
         <MessageCircle className="w-16 h-16" />
         <h1 className="heading-main">Interview Agent</h1>
         <p className="description-text">Sign in to start chatting with our AI-powered assistant</p>
